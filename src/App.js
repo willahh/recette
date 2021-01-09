37dbc7f8d53bdf58f9b54de3de5f5c65c78f38ed
wravel <wravel@gmail.com>
2021-01-09 19:59:51 +0100

    Add admin and card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 // CSS
 import './App.css'
+import Header from './components/Header';
+import recettes from './recettes';
+import Admin from './components/Admin';
+import Cards from './components/Card';
 
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
   }
 
+  chargerExemple = () => this.setState({ recettes });
+
   render () {
+    const cards = Object.keys(this.state.recettes)
+      .map(key => <Cards key={key} details={this.state.recettes[key]}></Cards>);
+
     return (
       <div className='box'>
-        <h1>Bonjour {this.state.pseudo}</h1>
-        <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
-        </div>
+        <Header pseudo={this.state.pseudo}></Header>
+        <h1>Bonjour  {this.state.pseudo}</h1>
+        { cards }
+        <Admin
+         chargerExemple={ this.chargerExemple }></Admin>
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = ({ pseudo }) => {
+  const formatPseudo = pseudo => /[aeiouy]/i.test(pseudo[0]) ? `d'${pseudo}` : `de ${pseudo}`;
+  console.log(formatPseudo(pseudo));
+
+  return (
+    <header>
+      <h1>La boîte à recettes {formatPseudo(pseudo)}</h1>
+    </header>
+  )
+}
+
+export default Header;
