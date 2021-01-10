693ab639588479a819ee9bf846ced27dfa6631e3
wravel <wravel@gmail.com>
2021-01-10 02:05:37 +0100

    Format

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,58 +1,55 @@
-import React, { Component } from "react"
-import AjouterRecette from "./AjouterRecette"
-import AdminForm from './AdminForm'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import base, { firebaseApp } from '../base'
-import Login from './Login'
+import React, { Component } from "react";
+import AjouterRecette from "./AjouterRecette";
+import AdminForm from "./AdminForm";
+import firebase from "firebase/app";
+import "firebase/auth";
+import base, { firebaseApp } from "../base";
+import Login from "./Login";
 
 class Admin extends Component {
   state = {
     uid: null,
-    chef: null
-  }
+    chef: null,
+  };
 
-  handleAuth = async authData => {
-    console.log('handleAuth', authData);
-    const box = await base.fetch(this.props.pseudo, { context: this })
+  handleAuth = async (authData) => {
+    console.log("handleAuth", authData);
+    const box = await base.fetch(this.props.pseudo, { context: this });
 
     if (!box.chef) {
       await base.post(`${this.props.pseudo}/chef`, {
-        data: authData.user.uid
-      })
+        data: authData.user.uid,
+      });
     }
-
-    console.log('box', box)
 
     this.setState({
       uid: authData.user.uid,
-      chef: box.chef || authData.user.uid
-    })
-
-  }
+      chef: box.chef || authData.user.uid,
+    });
+  };
 
   authenticate = () => {
-    const authProvider = new firebase.auth.FacebookAuthProvider()
+    const authProvider = new firebase.auth.FacebookAuthProvider();
     firebaseApp
       .auth()
       .signInWithPopup(authProvider)
-      .then(this.handleAuth)
-  }
+      .then(this.handleAuth);
+  };
 
   logout = async () => {
-    console.log('Déconnexion')
-    await firebase.auth().signOut()
-    this.setState({ uid: null })
-  }
+    console.log("Déconnexion");
+    await firebase.auth().signOut();
+    this.setState({ uid: null });
+  };
 
   render() {
     const { recettes, ajouterRecette, majRecette, chargerExemple } = this.props;
 
-    const logout = <button onClick={this.logout}>Déconnexion</button>
+    const logout = <button onClick={this.logout}>Déconnexion</button>;
 
     // Si l'utilisateur n'est pas connecté
     if (!this.state.uid) {
-      return <Login authenticate={this.authenticate}></Login>
+      return <Login authenticate={this.authenticate} />;
     }
 
     if (this.state.uid != this.state.chef) {
@@ -61,22 +58,21 @@ class Admin extends Component {
           <p>Tu n'es pas le chef de cette boîte !</p>
           {logout}
         </div>
-      )
+      );
     }
 
     return (
       <div className="cards">
         <div className="cards">
           <AjouterRecette ajouterRecette={ajouterRecette} />
-          {
-            Object.keys(recettes)
-              .map(key => <AdminForm
-                            key={key}
-                            id={key}
-                            majRecette={majRecette}
-                            recettes={recettes}>
-                            </AdminForm>)
-          }
+          {Object.keys(recettes).map((key) => (
+            <AdminForm
+              key={key}
+              id={key}
+              majRecette={majRecette}
+              recettes={recettes}
+            />
+          ))}
         </div>
         <footer>
           {logout}
